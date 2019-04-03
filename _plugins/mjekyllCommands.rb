4e24c6e6c4936f1cec014bfa91eb704463b2cbdc
Stephen Gaito <stephen@perceptisys.co.uk>
2019-04-03 12:58:08 +0100

    refactored blog and wiki commands to allow them to reside in common plugins

---
+++ _plugins/mjekyllCommands.rb
@@ -0,0 +1,9 @@
+# This mjekyll ruby plugin loads the mjekyll plugins required for this 
+# mjekyll sub-website.
+
+# The following order is CRITICAL!
+
+require 'blogCategoryNormalizer'
+require 'indexBlogPages'
+require 'newBlogCommands'
+
